2d5cd35656882e98eddd39069ba993412ca98510
boamahep <boamahep@gmail.com>
2020-09-01 16:29:33 +0000

    procfile exit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join('build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join("build", "index.html"));
     });
   }
 
